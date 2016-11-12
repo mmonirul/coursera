7fbd097b003aff017ad7b1d774588ee56bd62855
mmonirul <abc@abc.abc>
2016-11-12 01:32:28 +0100

    modified

--- assignment/assignment-1/app.js
+++ assignment/assignment-1/app.js
@@ -2,20 +2,20 @@
 (function () {
   'use strict';
 
-  angular.module('MealChecker', [])
-  .controller('MealCheckController', function ($scope) {
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', function ($scope) {
     $scope.meal = "";
     $scope.totalMealCount = '';
     $scope.myMeals = '';
 
     $scope.checkMyMeal = function () {
-      var totalMealItems = calculatNumericForString($scope.meal);
+      var totalMealItems = lunchItems($scope.meal);
       var myMeals = totalMealItems.split(",");
       $scope.myMeals = myMeals;
       $scope.totalMealCount = myMeals.length;
     };
 
-    function calculatNumericForString(string) {
+    function lunchItems(string) {
         var myMeal = string.replace(/^\s\s*/, '')
                             .replace(/\s\s*$/, '')
                             .replace(/[\s,]+/g, ',');
